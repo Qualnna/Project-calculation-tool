0fed4a880ac84bea693afcd20315984caee9309a
Bishobos <mps.kajkage@gmail.com>
2026-05-05 10:45:46 +0200

    initial commit

---
+++ src/main/java/qualnna/dascalculator/DasCalculatorApplication.java
@@ -0,0 +1,13 @@
+package qualnna.dascalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DasCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DasCalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/qualnna/dascalculator/DasCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package qualnna.dascalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DasCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

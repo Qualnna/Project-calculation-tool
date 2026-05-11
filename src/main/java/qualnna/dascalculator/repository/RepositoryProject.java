50aea46eacc34b24d5a9dcd47af1ced50ba36ad6
Bishobos <mps.kajkage@gmail.com>
2026-05-11 14:56:08 +0200

    added Connection object on repository field

--- src/main/java/qualnna/dascalculator/repository/RepositoryProject.java
+++ src/main/java/qualnna/dascalculator/repository/RepositoryProject.java
@@ -1,7 +1,25 @@
 package qualnna.dascalculator.repository;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.jdbc.datasource.lookup.DataSourceLookupFailureException;
 import org.springframework.stereotype.Repository;
+import qualnna.dascalculator.model.Project;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
 
 @Repository
 public class RepositoryProject {
+    public RepositoryProject(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+        if (jdbcTemplate.getDataSource() != null) {
+            this.connection = DataSourceUtils.getConnection(jdbcTemplate.getDataSource());
+        }
+        //Replace with custom exception
+        else{throw new DataSourceLookupFailureException("Datasource error");}
+    }
+
+    private final JdbcTemplate jdbcTemplate;
+    private final Connection connection;
 }

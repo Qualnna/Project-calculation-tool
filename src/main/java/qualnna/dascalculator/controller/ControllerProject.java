b0d1e33f113b158cad197bbfbde29772a4380022
Namirah MR <nare0001@stud.ek.dk>
2026-05-11 15:50:06 +0200

    getMapping and HTML

--- src/main/java/qualnna/dascalculator/controller/ControllerProject.java
+++ src/main/java/qualnna/dascalculator/controller/ControllerProject.java
@@ -1,9 +1,30 @@
 package qualnna.dascalculator.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import qualnna.dascalculator.model.Employee;
+import qualnna.dascalculator.service.ServiceProject;
 
 @Controller
 @RequestMapping("/")
 public class ControllerProject {
+    private final ServiceProject service;
+    public ControllerProject(ServiceProject service) {this.service = service;}
+
+    @GetMapping("/employee/create")
+    public String createEmployee(Model model) {
+        Employee newEmployee = new Employee();
+        model.addAttribute("newEmployee", newEmployee);
+        return "create-employee";
+    }
+    @PostMapping("/employee/add")
+    public String addEmployee(@ModelAttribute Employee newEmployee) {
+
+        return "redirect:/";
+    }
+
 }
--- src/main/java/qualnna/dascalculator/model/Employee.java
+++ src/main/java/qualnna/dascalculator/model/Employee.java
@@ -1,4 +1,54 @@
 package qualnna.dascalculator.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Employee {
+    private int empId;
+    private String name;
+    private double hourlyRate;
+    private List<String> skills = new ArrayList<>();
+    private List<Task> assignedTask = new ArrayList<>();
+
+    public Employee() {}
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public void setHourlyRate(double hourlyRate) {
+        this.hourlyRate = hourlyRate;
+    }
+
+    public List<String> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(List<String> skills) {
+        this.skills = skills;
+    }
+
+    public List<Task> getAssignedTask() {
+        return assignedTask;
+    }
+
+    public void setAssignedTask(List<Task> assignedTask) {
+        this.assignedTask = assignedTask;
+    }
 }

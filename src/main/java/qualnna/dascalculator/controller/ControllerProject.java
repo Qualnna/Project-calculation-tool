66a10c50bd9c399be7cd5c23d6f883c82933de68
Bishobos <mps.kajkage@gmail.com>
2026-05-15 16:05:37 +0200

    made sure to add newly created project to session

--- src/main/java/qualnna/dascalculator/controller/ControllerProject.java
+++ src/main/java/qualnna/dascalculator/controller/ControllerProject.java
@@ -8,18 +8,27 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import qualnna.dascalculator.exceptions.InvalidDateException;
+import qualnna.dascalculator.model.Employee;
 import qualnna.dascalculator.model.Project;
 import qualnna.dascalculator.service.ServiceProject;
 
 import java.sql.SQLException;
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
 public class ControllerProject {
 
     private final ServiceProject service;
+    private List<Employee> employees;
+    private List<String> skills;
+    private Project project;
 
     ControllerProject(ServiceProject service){this.service = service;}
+
+    private boolean validateSession(HttpSession session){
+        return session!=null;
+    }
 
     @GetMapping("/addProject")
     public String addProjectGet(Model model, HttpSession session){
@@ -32,7 +41,8 @@ public class ControllerProject {
     @PostMapping("/addProject")
     public String addProjectPost(@ModelAttribute Project projectToAdd, Model model, HttpSession session){
         try {
-            model.addAttribute("project", service.addProject(projectToAdd));
+            this.project = service.addProject(projectToAdd);
+            session.setAttribute("project", this.project);
             return "redirect:/show-project";
         } catch (InvalidDateException e) {
             model.addAttribute("project", projectToAdd);

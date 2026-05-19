4739037817a9879a2a6118194b939f5884ef756d
Bishobos <mps.kajkage@gmail.com>
2026-05-19 10:57:37 +0200

    removed print from test

--- src/test/java/qualnna/dascalculator/repository/RepositoryProjectTest.java
+++ src/test/java/qualnna/dascalculator/repository/RepositoryProjectTest.java
@@ -78,7 +78,6 @@ class RepositoryProjectTest {
     @Test
     void readAllEmployees() throws Exception{
         List<Employee> employeeList = repository.readEmployees();
-        System.out.println(employeeList);
     }
 
     @Test
